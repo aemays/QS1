1c121e5e9cf8d8520251f293d5fe8ccbfabb4f5b
Anthony Mays <aemays@LAPTOP-7V4T9VDE>
2025-05-15 13:17:32 -0400

    nothing

--- setup.py
+++ setup.py
@@ -3,7 +3,8 @@ from setuptools import setup, find_packages
 setup(
     name="montecarlo",
     version="0.1.0",
-    packages=find_packages(),
+    packages=find_packages(where="montecarloPackage"),
+    package_dir={"": "montecarloPackage"},
     install_requires=[
         # Add your dependencies here
     ],
