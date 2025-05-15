5ae58711fe56294218cd44489dc4e258b417236c
Anthony Mays <aemays@LAPTOP-7V4T9VDE>
2025-05-15 06:56:10 -0400

    trying to initialize the isingHamiltonian module in the montecarlo package

---
+++ assignments/montecarlo/montecarlo/__init__.py
@@ -0,0 +1,8 @@
+"""This file will turn a directory into a python package"""
+
+# Add imports here
+from .functions import *
+from .bitString import *
+from .isingHamiltonian import *
+
+from ._version import __version__
